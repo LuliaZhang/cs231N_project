31eb9616bc829d814e0cbf0ed12eca5b5c67fa39
LuliaZhang <Lulia_123@outlook.com>
2022-05-22 11:59:13 -0700

    Update efficientnet_naive.py

--- efficientnet_naive.py
+++ efficientnet_naive.py
@@ -9,7 +9,9 @@ import PIL.Image
 import matplotlib.pyplot as plt
 from sklearn.utils import class_weight
 from sklearn.metrics import confusion_matrix, cohen_kappa_score
+import tensorflow.keras as keras
 from tensorflow import keras
+
 from keras import backend as K
 from tensorflow.keras.models import Model
 from keras import optimizers, applications
